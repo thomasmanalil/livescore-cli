3f4f3cc11dcd912e4bb81e217db7205ea7073dc7
anuj <anuj.gautam@logpoint.com>
2017-08-10 16:55:28 +0545

    Fixed the match details reader for the given use cases

--- lib/lsreader.py
+++ lib/lsreader.py
@@ -6,8 +6,12 @@ def details_reader(details):
         score
         home_detail
         away_detail
+    #NOTE: Currently the method ignores first and last 3 rows because they contain
+           table header and footer and not the match detail itself.
+           I'd recommend passing only the match details and not the header and
+           footer, and removing the below line. 
     '''
-    details = details[:len(details) - 4]
+    details = details[1:len(details) - 4]
 
     def get_goal(goal_detail):
         details_dict = {}
@@ -26,7 +30,6 @@ def details_reader(details):
     def get_away_details(away_details):
         details_dict = {}
         if isinstance(away_details[0], list):
-            away_details[0], away_details[1] = away_details[1], away_details[0]
             details_dict.update(get_goal(away_details))
         else:
             details_dict[away_details[0]] = away_details[1]
@@ -48,12 +51,10 @@ def details_reader(details):
         return details_dict
     return map(read_row, details)
 
-'''
-Example use
-details = [[u" 4' ", [[u'Florin Andone', u'goal'], u' Guilherme (assist) '], u' 1 - 0 '], [u" 25' ", [u'Fernando Navarro', u'yellowcard'], u' \xa0 '], [u" 28' ", [[u'Florin Andone', u'goal'], u' Emre Colak (assist) '], u' 2 - 0 '], [u" 38' ", u' \xa0 ', [u'yellowcard', u'Roque Mesa']], [u" 39' ", [u'Carles Gil', u'goal'], u' 3 - 0 '], [u" 44' ", [u'Carles Gil', u'yellowcard'], u' \xa0 '], [u" 48' ", [u'Pedro Mosquera', u'yellowcard'], u' \xa0 '], [u" 51' ", u' \xa0 ', [u'yellowcard', u'Hernan Santana']], [u" 54' ", [u'Emre Colak', u'yellowcard'], u' \xa0 ', [u'yellowcard', u'Momo']], [u" 79' ", u' \xa0 ', [u'redyellowcard', u'Hernan Santana']], [u'venue :', u'spectators :'], [u'Estadio Municipal de Riazor', u'21764'], u'referee :', u'Mario Melero (Spain)']
-details_dict = details_reader(details)
-'''
-details = [[u'match details :', u'show assists'], [u" 6' ", u' 0 - 1 ', [[u'goal', u'Zinedine Machach'], u' (assist) Andy Delort ']], [u" 25' ", u' \xa0 ', [u'yellowcard', u'Ibrahim Sangare']], [u" 28' ", [[u'Jemerson', u'goal'], u' Joao Moutinho (assist) '], u' 1 - 1 '], [u" 30' ", u' \xa0 ', [u'yellowcard', u'Zinedine Machach']], [u" 49' ", [u'Joao Moutinho', u'yellowcard'], u' \xa0 '], [u" 53' ", u' 1 - 2 ', [[u'goal', u'Andy Delort'], u' (assist) Jimmy Durmaz ']], [u" 58' ", [[u'Radamel Falcao', u'goal'], u' Jorge (assist) '], u' 2 - 2 '], [u" 70' ", [[u'Kamil Glik', u'goal'], u' Joao Moutinho (assist) '], u' 3 - 2 '], [u" 81' ", u' \xa0 ', [u'yellowcard', u'Francois Moubandje']], [u'venue :', u'spectators :'], [u'Stade Louis II, Monaco', u'13572'], u'referee :', u'Clement Turpin (France)']
-details = [[u'match details :', u'show assists'], [u" 14' ", [[u'Nolan Roux', u'goal'], u' Renaud Cohade (assist) '], u' 1 - 0 '], [u" 39' ", [u'Milan Bisevac', u'yellowcard'], u' \xa0 '], [u" 39' ", u' 1 - 1 ', [u'goal', u' (pen.) ', u'Jimmy Briand']], [u" 41' ", u' \xa0 ', [u'yellowcard', u'Lebogang Phiri']], [u" 45' ", u' \xa0 ', [u'yellowcard', u'Yannis Salibur']], [u" 71' ", u' 1 - 2 ', [[u'goal', u'Ludovic Blas'], u' (assist) Pedro Rebocho ']], [u" 85' ", u' 1 - 3 ', [[u'goal', u'Mustapha Diallo'], u' (assist) Ludovic Blas ']], [u'venue :', u'spectators :'], [u'Stade Saint Symphorien', u'14595'], u'referee :', u'Jerome Miguelgorry (France)']
+details1 = [[u'match details :', u'show assists'], [u" 4' ", [[u'Florin Andone', u'goal'], u' Guilherme (assist) '], u' 1 - 0 '], [u" 25' ", [u'Fernando Navarro', u'yellowcard'], u' \xa0 '], [u" 28' ", [[u'Florin Andone', u'goal'], u' Emre Colak (assist) '], u' 2 - 0 '], [u" 38' ", u' \xa0 ', [u'yellowcard', u'Roque Mesa']], [u" 39' ", [u'Carles Gil', u'goal'], u' 3 - 0 '], [u" 44' ", [u'Carles Gil', u'yellowcard'], u' \xa0 '], [u" 48' ", [u'Pedro Mosquera', u'yellowcard'], u' \xa0 '], [u" 51' ", u' \xa0 ', [u'yellowcard', u'Hernan Santana']], [u" 54' ", [u'Emre Colak', u'yellowcard'], u' \xa0 ', [u'yellowcard', u'Momo']], [u" 79' ", u' \xa0 ', [u'redyellowcard', u'Hernan Santana']], [u'venue :', u'spectators :'], [u'Estadio Municipal de Riazor', u'21764'], u'referee :', u'Mario Melero (Spain)']
+details2 = [[u'match details :', u'show assists'], [u" 6' ", u' 0 - 1 ', [[u'goal', u'Zinedine Machach'], u' (assist) Andy Delort ']], [u" 25' ", u' \xa0 ', [u'yellowcard', u'Ibrahim Sangare']], [u" 28' ", [[u'Jemerson', u'goal'], u' Joao Moutinho (assist) '], u' 1 - 1 '], [u" 30' ", u' \xa0 ', [u'yellowcard', u'Zinedine Machach']], [u" 49' ", [u'Joao Moutinho', u'yellowcard'], u' \xa0 '], [u" 53' ", u' 1 - 2 ', [[u'goal', u'Andy Delort'], u' (assist) Jimmy Durmaz ']], [u" 58' ", [[u'Radamel Falcao', u'goal'], u' Jorge (assist) '], u' 2 - 2 '], [u" 70' ", [[u'Kamil Glik', u'goal'], u' Joao Moutinho (assist) '], u' 3 - 2 '], [u" 81' ", u' \xa0 ', [u'yellowcard', u'Francois Moubandje']], [u'venue :', u'spectators :'], [u'Stade Louis II, Monaco', u'13572'], u'referee :', u'Clement Turpin (France)']
+details3 = [[u'match details :', u'show assists'], [u" 14' ", [[u'Nolan Roux', u'goal'], u' Renaud Cohade (assist) '], u' 1 - 0 '], [u" 39' ", [u'Milan Bisevac', u'yellowcard'], u' \xa0 '], [u" 39' ", u' 1 - 1 ', [u'goal', u' (pen.) ', u'Jimmy Briand']], [u" 41' ", u' \xa0 ', [u'yellowcard', u'Lebogang Phiri']], [u" 45' ", u' \xa0 ', [u'yellowcard', u'Yannis Salibur']], [u" 71' ", u' 1 - 2 ', [[u'goal', u'Ludovic Blas'], u' (assist) Pedro Rebocho ']], [u" 85' ", u' 1 - 3 ', [[u'goal', u'Mustapha Diallo'], u' (assist) Ludovic Blas ']], [u'venue :', u'spectators :'], [u'Stade Saint Symphorien', u'14595'], u'referee :', u'Jerome Miguelgorry (France)']
 if __name__=='__main__':
-    print(details_reader(details))
+    print(details_reader(details1))
+    print(details_reader(details2))
+    print(details_reader(details3))
